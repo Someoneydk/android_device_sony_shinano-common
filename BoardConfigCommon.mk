595b262456890764945daa1a1f1fb8b3aacc94eb
Someoneydk <yt.sahulkrishan@gmail.com>
2018-03-10 17:36:25 +0100

    shinano-common: Add missing separators

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,14 +122,14 @@ NXP_CHIP_TYPE := 1
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /system/lib/libcammw.so|libsensor.so \
-    /system/lib/libfacenet.so|libprotobuf-cpp-shit.so
+    /system/lib/libfacenet.so|libprotobuf-cpp-shit.so \
     /system/lib/libsomc_chokoballpal.so|/system/vendor/lib/libGraphicBuffer.so \
-    /system/lib/hw/camera.vendor.qcom.so|libsensor.so 
+    /system/lib/hw/camera.vendor.qcom.so|libsensor.so \
     /system/lib/hw/camera.vendor.qcom.so|/system/vendor/lib/libsonycamera.so \
     /system/vendor/bin/credmgrd|/system/vendor/lib/libshims_signal.so \
     /system/vendor/bin/iddd|/system/vendor/lib/libshims_idd.so \
     /system/vendor/bin/mm-qcamera-daemon|libandroid.so \
-    /system/vendor/bin/suntrold|/system/vendor/lib/libshims_signal.so \
+    /system/vendor/bin/suntrold|/system/vendor/lib/libshims_signal.so
 
 # Filesystem
 BOARD_FLASH_BLOCK_SIZE := 131072
